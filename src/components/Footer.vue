56ed7d211e5b9d4b1c1766a232cbfdd0d0eda0a4
MyuB <mjycoco0@naver.com>
2022-01-28 19:28:45 +0900

    minor change

---
+++ src/components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <footer>
+    <Logo />
+    <a 
+      href="https://github.com/MyuB/vue3-project"
+      target="_blank">
+      (c){{ new Date().getFullYear() }} MyuB
+    </a>
+  </footer>
+</template>
+
+<script>
+import Logo from '~/components/Logo'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+footer {
+  padding: 70px 0;
+  text-align: center;
+  opacity: .2;
+  .logo {
+    display: block;
+    margin-bottom: 4px;
+  }
+  
+}
+</style>
